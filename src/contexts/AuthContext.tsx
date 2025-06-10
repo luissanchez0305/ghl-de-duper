c78fe3ca64897255bd320ca555fe6a387e11b897
Luis Ricardo Sanchez <luis.sanchez@esferasoluciones.com>
2025-06-10 17:33:57 -0500

    test client id

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -68,6 +68,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const initiateGHLAuth = () => {
     console.log('Initiating GHL OAuth flow');
     console.log('Redirect URI:', GHL_REDIRECT_URI);
+    console.log('Client ID:', GHL_CLIENT_ID);
     const authUrl = `https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&client_id=${GHL_CLIENT_ID}&redirect_uri=${encodeURIComponent(GHL_REDIRECT_URI)}`;
     
     // Open OAuth in a popup window
